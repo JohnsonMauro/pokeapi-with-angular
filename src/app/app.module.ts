4248824f946d5ddff237fb07e2f7aff968ad6a65
JohnsonMauro <johnsonmauro@gmail.com>
2021-09-02 10:24:00 -0300

    add ngx-toastr for error handler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { RoutesModule } from './routes/routes.module';
@@ -8,11 +9,13 @@ import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { SharedModule } from './shared/shared.module';
 import { ModalsComponent } from './components/modal/modal-content.component';
-
+import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [AppComponent, ModalsComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
     HttpClientModule,
     SharedModule,
     LayoutModule,
--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ToastrService } from 'ngx-toastr';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalsComponent } from '@components/modal/modal-content.component';
 
@@ -17,38 +18,45 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private modalService: NgbModal,
+    private toastr: ToastrService,
     private pokeApiService: PokeApiService
   ) {}
 
   ngOnInit() {
-    this.pokeApiService.getPokemonList().subscribe((data) => {
-      this.nextPokemons = data.next;
-      this.totalPokemons = data.count;
-      this.pokemonList = data.results;
+    this.pokeApiService.getPokemonList().subscribe({
+      next: (data) => {
+        this.nextPokemons = data.next;
+        this.totalPokemons = data.count;
+        this.pokemonList = data.results;
+      },
+      error: (error) => this.toastr.error(error.error, 'Alert!'),
     });
   }
 
   public getPokemonByName(name: string) {
     if (name) {
-      this.pokeApiService
-        .getPokemonByName(name)
-        .subscribe((data) => this.openModalPokemon(data));
+      this.pokeApiService.getPokemonByName(name).subscribe({
+        next: (data) => this.openModalPokemon(data),
+        error: (error) => this.toastr.error(`Pokemon ${error.error}`, 'Alert!'),
+      });
     }
   }
 
   public getNextPokemonList() {
-    this.pokeApiService
-      .getPokemonDetails(this.nextPokemons)
-      .subscribe((data) => {
+    this.pokeApiService.getPokemonDetails(this.nextPokemons).subscribe({
+      next: (data) => {
         this.nextPokemons = data.next;
         this.pokemonList.push(...data.results);
-      });
+      },
+      error: (error) => this.toastr.error(error.error, 'Alert!'),
+    });
   }
 
   public openPokemonDetails(url: string) {
-    this.pokeApiService
-      .getPokemonDetails(url)
-      .subscribe((data) => this.openModalPokemon(data));
+    this.pokeApiService.getPokemonDetails(url).subscribe({
+      next: (data) => this.openModalPokemon(data),
+      error: (error) => this.toastr.error(error.error, 'Alert!'),
+    });
   }
 
   private openModalPokemon(details: any) {
